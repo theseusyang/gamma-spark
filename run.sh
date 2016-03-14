336b8f8e967fb26b41717b12b6ffdda4c54be34a
Yiqun Zhang <zhangyiqun9164@gmail.com>
2016-03-14 15:38:46 -0500

    run.sh can be executed from other dirctory

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ if [ $# -ne 1 ]; then
   echo "run.sh \"hdfs://node1:54310/KDDn100Md38_Y.csv\""
   exit -1;
 fi
+cd "$( dirname "${BASH_SOURCE[0]}" )"
 spark-submit --class GammaSpark target/scala-2.10/gammaspark_2.10-1.0.jar $1
